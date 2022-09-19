66b33d39fd59f879079aca8b3f8754f4bc642734
Crédo Junior GBEDEDJI <cjgbededji@groupe-igs.fr>
2022-09-19 17:17:18 +0200

    Challenge: Création d'un controlleur qui renvoit la un film en fonction de l'id de ce dernier

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    const MOVIES = [
+        [
+            'name' => 'Godfather',
+            'description' => "Un film qui raconte la vie d'un jeune garçon élevé par un père mafieux et qui a voulu suivre un autre chemin",
+            'year' => 2001
+        ],
+        [
+            'name' => 'Blackpanther',
+            'description' => "Un film incroyable de Marvel Studio qui met en scène un héro incroyable",
+            'year' => 2013
+        ],
+        [
+            'name' => 'Lord of rings',
+            'description' => "Le seigneur des anneaux",
+            'year' => 2000
+        ]
+    ];
+
+    #[Route('/movies/{id}', name: 'app_movies_index', requirements: ['id' => '\d+'])]
+    public function show(int $id): Response
+    {
+        if(!array_key_exists($id, self::MOVIES)){
+            throw $this->createNotFoundException("Le film n'existe pas !");
+        }
+
+        $movie = self::MOVIES[$id];
+
+        return $this->json($movie);
+    }
+}
